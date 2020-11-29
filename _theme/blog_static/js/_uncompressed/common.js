6e3bcf420238dfd2a302413adab89a3ad0954579
WebbingStudio <mail@webbingstudio.com>
2020-11-29 18:40:51 +0900

    slick.jsを追加

--- _theme/blog_static/js/_uncompressed/common.js
+++ _theme/blog_static/js/_uncompressed/common.js
@@ -6,3 +6,21 @@
     });
 
 }());
+
+(function($){
+$(function(){
+
+// slick
+$('.echo-mt-be-slider .js-slider')
+  .slick({
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    speed: 1500,
+    autoplaySpeed: 5000,
+    pauseOnHover: false
+  });
+
+}); // end document.ready
+})(jQuery); // end jquery
