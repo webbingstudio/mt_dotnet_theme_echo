388ad1b84e588964f8e7232713eb6a2b84f662ea
WebbingStudio <mail@webbingstudio.com>
2021-01-11 14:05:26 +0900

    .mt-be-columns のカラム数を計測するJSを追加

--- _theme/blog_static/js/_uncompressed/common.js
+++ _theme/blog_static/js/_uncompressed/common.js
@@ -3,6 +3,18 @@
 
     document.addEventListener('DOMContentLoaded', function() {
         new ScrollHint('.js-scrollable');
+
+        var be = document.querySelector('.echo-mt-be');
+        var el = be.querySelectorAll('.mt-be-columns');
+        if(el.length > 0) {
+            el.forEach(function(columns) {
+                var column = columns.querySelectorAll('.mt-be-column');
+                if(column.length > 0) {
+                    columns.classList.add('echo-mt-be-columns-' + column.length);
+                }
+            });
+        }
+
     });
 
 }());
